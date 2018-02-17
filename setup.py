1922c7a8dfc0937adac7b67c34cd792f8040fd73
Regev Golan <regev@hiredscore.com>
2018-02-17 17:40:24 +0200

    [FEAT] Make package

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='nn_tools',
+      version='0.1',
+      description='Neural network infra and tools',
+      packages=['nn_tools'],
+      zip_safe=False)
